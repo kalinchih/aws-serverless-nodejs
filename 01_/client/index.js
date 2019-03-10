60bacf437022e8086d6d2de93c6d1a8faf07891b
Kalin <kalinchih@gmail.com>
2019-03-10 14:57:21 +0800

    enhance client js

--- 01_/client/index.js
+++ 01_/client/index.js
@@ -1,12 +1,21 @@
-// run in https://codepen.io
+//// run in https://codepen.io
 
-fetch(
-  "https://gghd87h0s9ug.execute-api.ap-northeast-1.amazonaws.com/dev/comparisons",
-  {
-    method: "POST" // *GET, POST, PUT, DELETE, etc.
-    //mode: 'cors' // no-cors, cors, *same-origin
-  }
-)
+const url =
+  "https://gghd87h0s9ug.execute-api.ap-northeast-1.amazonaws.com/dev/comparisons";
+
+//// ES6
+fetch(url, {
+  method: "POST" // *GET, POST, PUT, DELETE, etc.
+  //mode: 'cors' // no-cors, cors, *same-origin
+})
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(error => console.log(error.name, error.message));
+
+//// ES5
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+xhr.onreadystatechange = function(event) {
+  console.log(event.target);
+};
+xhr.send();
